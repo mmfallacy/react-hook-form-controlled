6a3f2408b9f32687db7774c5ec7f682a79791ff6
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-01-22 20:18:29 +0800

    CHORE: Babel plugin for easier styled-components debugging using React DevTools

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import backgroundSVG from './background.svg'
 
 export const Page = styled.div`
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,5 +1,5 @@
 
-import {createGlobalStyle} from 'styled-components'
+import {createGlobalStyle} from 'styled-components/macro'
 export const GlobalStyles = createGlobalStyle`
     html, body, #root{
         height:100%;
