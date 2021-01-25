d670c5647f1cd9e414f4257b176c53ca8afa987f
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-01-25 16:48:48 +0800

    UI: Style import for Roboto Google Font

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,14 +1,17 @@
 
 import {createGlobalStyle} from 'styled-components/macro'
 export const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
     html, body, #root{
         height:100%;
         width: 100%;
         margin: 0;
         padding: 0;
+        font-family: Roboto;
     }
 
     *{
         box-sizing: border-box;
+        letter-spacing: .1px;
     }
 `
