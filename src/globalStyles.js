460638f244ab406463b8a6c94e9594af5a1f1959
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-01-30 15:36:39 +0800

    FIX: Move font imports to index.html instead of global styles due to warning

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,7 +1,6 @@
 
 import {createGlobalStyle} from 'styled-components/macro'
 export const GlobalStyles = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
     html, body, #root{
         height:100%;
         width: 100%;
